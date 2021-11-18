463ad117f2c9db4853cd703a30f99586457d4ef2
HuangLian126 <51012563+HuangLian126@users.noreply.github.com>
2021-11-18 19:27:10 +0800

    Delete demo_voc_standard_coco.sh

--- tools/fewshot_exp/demo_voc_standard_coco.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
-export NGPUS=1
-SHOT=(10)
-for shot in ${SHOT[*]}
-do
-  configfile=configs/fewshot/standard/e2e_coco_${shot}shot_finetune.yaml
-  python -m torch.distributed.launch --nproc_per_node=$NGPUS ./tools/demo.py --config-file ${configfile}
-done
