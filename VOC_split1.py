bae540a2f5d398ab35b3aefb9fc6754eba8dc9e0
HuangLian126 <51012563+HuangLian126@users.noreply.github.com>
2021-11-18 19:25:04 +0800

    Update VOC_split1.py

--- VOC_split1.py
+++ VOC_split1.py
@@ -26,22 +26,5 @@ t = torch.empty(change[1][1])
 torch.nn.init.constant_(t, 0)
 model[change[1][0]] = t
 
-'''
-change2 = [('sup_classfication.weight', (20, 1024)), ('sup_classfication.bias', 20)]
-t = torch.empty(change2[0][1])
-torch.nn.init.normal_(t, std=0.01)
-model[change2[0][0]] = t
-
-t = torch.empty(change2[1][1])
-torch.nn.init.constant_(t, 0)
-model[change2[1][0]] = t
-
-change3 = [('weight', (20, 1024))]
-t = torch.empty(change3[0][1])
-torch.nn.init.normal_(t, std=0.01)
-model[change3[0][0]] = t
-
-'''
-
 checkpoint = dict(model=model)
 torch.save(checkpoint, 'voc0712_split_1_base_pretrained.pth')
