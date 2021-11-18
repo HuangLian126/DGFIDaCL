9a443adfe83794ca5da1ee7d7959560cbcca7e9c
HuangLian126 <51012563+HuangLian126@users.noreply.github.com>
2021-11-18 19:27:31 +0800

    Delete train_coco_base.sh

--- tools/fewshot_exp/train_coco_base.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
-export NGPUS=1
-configfile=configs/fewshot/base/e2e_coco_base.yaml
-python -m torch.distributed.launch --nproc_per_node=$NGPUS ./tools/train_net.py --config-file ${configfile}
